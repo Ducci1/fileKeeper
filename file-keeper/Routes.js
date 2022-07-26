62a504bedf92ac822546357b92520ac188e35340
ducci1 <dubemellis1@gmail.com>
2022-07-26 14:16:17 +0100

    Updated

--- file-keeper/Routes.js
+++ file-keeper/Routes.js
@@ -1,19 +1,45 @@
 const express = require('express')
 const route = express.Router()
+const multer = require('multer')
+const thisUpload = require('./Models')
+const fs = require('fs')
+const path = require('path')
+const { send } = require('process')
+const upload = multer({dest: './file-keeper/Images'})
 
-route.post('/api/create' , (req , res)=>{
+
+route.post('/create', upload.single("image") , async(req , res)=>{
+    const obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/Images/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    await thisUpload.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            item.save();
+            res.redirect('/');
+        }
+    });
+
+    
     
 })
 
-route.post('/api/read' , (req , res)=>{
+route.post('/read' , (req , res)=>{
 
 })
 
-route.post('/api/modify' , (req , res)=>{
+route.post('/modify' , (req , res)=>{
 
 })
 
-route.post('/api/delete' , (req , res)=>{
+route.post('/delete' , (req , res)=>{
 
 })
 
--- file-keeper/file-keeper.js
+++ file-keeper/file-keeper.js
@@ -1,21 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./Routes')
-
+const routes = require('./routes')
+const path = require('path')
+const multer = require('multer')
+const parser = require('body-parser')
 const app = express()
-const PORT = '3000'
+
+
+mongoose.connect('mongodb://localhost:27017/uploads')
+
+
+app.use('/' , express.static(path.join(__dirname,'Frontend')))
+app.use(parser.json())
+// const storage = multer.diskStorage({
+//     destination: (req , file, cb)=>{
+//         cb(null, 'Images')
+//     },
+//     filename: (req, file, cb)=>{
+//         console.log(file)
+//         cb('null', Date.now() + path.extname(file.originalname))
+//     }
+// })
+
+// const upload = multer({storage: storage})
 
 app.get('/' , (req , res)=>{
-    
+    res.sendFile(path.join( __dirname ,'Frontend' , 'fileUp.html'))
 })
 
-app.post('/' , (req , res)=>{
-    const records = req.params
-    console.log(records)
-})
+app.use('/routes' , routes)
 
 
 
+const PORT = '3000'
 app.listen(PORT , ()=>{
     console.log('Server is listening at port 3000')
 })
